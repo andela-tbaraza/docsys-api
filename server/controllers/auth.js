472285fd46ac5049e8f6c87df10e11fb8ed710cc
Tonida Baraza <tonida.baraza@andela.com>
2016-08-30 20:27:22 +0300

    put authenticate method to auth folder

---
+++ server/controllers/auth.js
@@ -0,0 +1,52 @@
+const User = require('../models/users');
+const jwt = require('jsonwebtoken');
+const config = require('../../config');
+
+module.exports = {
+
+  authenticateUser: function(req, res) {
+    // find the user by name, username and password
+    User.findOne({
+      username: req.body.username
+    }).select('email username password').exec(function(err, user) {
+      if (err) {
+        throw err;
+      }
+
+      // if the user with that username is not found
+      if (!user) {
+        res.json({
+          success: false,
+          message: 'Authentication failed. User not found'
+        });
+
+      } else if (user) {
+        // compare the password to see if they match
+        let validPassword = user.comparePassword(req.body.password);
+        if (!validPassword) {
+          res.json({
+            success: false,
+            message: 'Authentication failed. Wrong password'
+          });
+        }
+        // if authentication is successful
+        else {
+          // create a token
+          let token = jwt.sign({
+            username: user.username,
+            email: user.email
+          }, config.secret, {
+            expiresIn: '24h' // token expires in 24 hrs
+          });
+
+          // return this data including the token
+          res.json({
+            success: true,
+            message: 'here is your token',
+            token: token
+          });
+        }
+      }
+    });
+  }
+};
