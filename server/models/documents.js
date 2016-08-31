0e2c0037a80f8f990410e2810ae290c72e1a1d96
Tonida Baraza <tonida.baraza@andela.com>
2016-09-01 00:11:29 +0300

    add documents model

--- server/models/documents.js
+++ server/models/documents.js
@@ -1,1 +1,27 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+// const User = require('./users');
+
+const DocumentSchema = new Schema({
+  ownerId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  title: {
+    type: String
+    // unique: true
+  },
+  content: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  modifiedAt: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+module.exports = mongoose.model('Document', DocumentSchema);
