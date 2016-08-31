0c906642f9c7ae380d6b6f1a7ae73d18ad6ab228
Tonida Baraza <tonida.baraza@andela.com>
2016-09-01 00:18:21 +0300

    add GET route

--- server/routes/documents/documentRoutes.js
+++ server/routes/documents/documentRoutes.js
@@ -6,8 +6,8 @@ const controller = require('../../controllers/documents');
 router.post('/documents', controller.create);
 
 //GET /documents
-// router.get('/documents', controller.find);
-//
+router.get('/documents', controller.find);
+
 // // GET /documents/:document_id
 // router.get('/documents/:document_id', controller.findDocument);
 //
