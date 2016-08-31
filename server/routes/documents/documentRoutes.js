161950395a2bbbbd376ad8926db21164ac9033da
Tonida Baraza <tonida.baraza@andela.com>
2016-09-01 00:13:59 +0300

    add POST route

---
+++ server/routes/documents/documentRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../../controllers/documents');
+
+// POST /documents
+router.post('/documents', controller.create);
+
+//GET /documents
+// router.get('/documents', controller.find);
+//
+// // GET /documents/:document_id
+// router.get('/documents/:document_id', controller.findDocument);
+//
+// // PUT  /documents/:document_id
+// router.put('/documents/:document_id', controller.updateDocument);
+
+module.exports = router;
