6983e346d4f55543210235c5209ab00b27114c6b
Tonida Baraza <tonida.baraza@andela.com>
2016-09-08 17:21:50 +0300

    change access middleware

--- server/routes/users/userRoutes.js
+++ server/routes/users/userRoutes.js
@@ -1,20 +1,21 @@
 const controller = require('../../controllers/users');
-const hasAccess = require('../../middlewares/access');
+const daemon = require('../../middlewares');
 
 module.exports = (router) => {
   // GET /users
-  router.get('/users', hasAccess(['admin']), controller.retrieve);
+  router.get('/users', daemon.hasAccess(['admin'], 'userId', 'params'), controller.retrieve);
 
   // GET /users/:user_id
-  router.get('/users/:user_id', hasAccess(['user', 'admin']), controller.findUser);
+  router.get('/users/:user_id', daemon.hasAccess(['user', 'admin'], 'userId', 'params'), controller.findUser);
 
   // PUT /users/:user_id
-  router.put('/users/:user_id', hasAccess(['user', 'admin']), controller.updateUser);
+  router.put('/users/:user_id', daemon.hasAccess(['user', 'admin'], 'userId', 'params'), controller.updateUser);
 
   // DELETE /users/:user_id
-  router.delete('/users/:user_id', hasAccess(['user', 'admin']), controller.deleteUser);
+  router.delete('/users/:user_id', daemon.hasAccess(['user', 'admin'], 'userId', 'params'), controller.deleteUser);
 
   // GET /users/:id/documents
-  router.get('/users/:id/documents', hasAccess(['admin', 'user']), controller.findDocuments);
+  router.get('/users/:id/documents', daemon.hasAccess(['admin', 'user'], 'userId', 'params'), controller.findDocuments);
 
 };
+// daemon
