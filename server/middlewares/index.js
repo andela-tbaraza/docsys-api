9ce0a69ebf85dc70697d908de1f6270ea18b9543
Tonida Baraza <tonida.baraza@andela.com>
2016-09-09 13:00:43 +0300

    fix access errors

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,14 +1,21 @@
+const Document = require('../models/documents');
+
 module.exports ={
   hasAccess: function hasAccess(accessLevel, userId, params) {
     return function(req, res, next) {
-      for (let level of accessLevel) {
-        if ( level === 'user') {
+      if (accessLevel.indexOf(req.decoded.title) > -1) {
+        if (req.decoded.title === 'user') {
           params = req.params.user_id;
           userId = req.decoded._id;
 
-          if(userId === params) {
+          if(userId !== params) {
+            res.send({
+              message: 'Not authorized'
+            });
+          } else {
             return next();
           }
+
         } else {
           return next();
 
@@ -17,5 +24,48 @@ module.exports ={
       }
     };
 
+  },
+
+  docAccess: function(accessLevel, params) {
+    return function(req, res, next) {
+      params = req.params.document_id;
+      Document.findById(params).select('ownerId').exec(function(err, id) {
+        if(err) {
+          throw err;
+        } else {
+          if (accessLevel.indexOf(req.decoded.title) >  -1) {
+            if (req.decoded.title === 'user') {
+              let ownerId = id.ownerId;
+              if (ownerId !== req.decoded._id) {
+                res.send({
+                  success: false,
+                  message: 'Not authorized'
+                });
+              } else {
+                return next();
+              }
+
+            } else {
+              return next();
+            }
+          }
+        }
+      });
+    };
+  },
+
+  userAccess: function userAccess(accessLevel) {
+    return function (req, res, next) {
+      if (accessLevel.indexOf(req.decoded.title) >  -1) {
+        return next();
+      } else {
+        return res.json({
+          success: false,
+          message: 'Not authorized'
+        });
+      }
+
+    };
   }
+
 };
