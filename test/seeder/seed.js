bec000a9ac67a9758d8fa06e93f0eb766a37a415
Tonida Baraza <tonida.baraza@andela.com>
2016-09-13 09:57:46 +0300

    fix seeding error

---
+++ test/seeder/seed.js
@@ -0,0 +1,45 @@
+const seeder = require('mongoose-seed');
+const config = require('../../config.js');
+const path = require('path');
+
+// Data array containing seed data - documents organized by Model
+var data = [
+  {
+    'model': 'User',
+    'documents': [
+      {
+        'name.first': 'tonida',
+        'name.last': 'baraza',
+        'username': 'tonee',
+        'email': 'toni@gmail.com',
+        'title': 'admin',
+        'password': 'admin'
+
+      }
+
+    ]
+  }
+];
+
+module.exports = {
+  // Connect to MongoDB via Mongoose
+
+  seeder: seeder.connect(config.test.url, function() {
+
+    // Load Mongoose models
+    seeder.loadModels([
+      path.join(__dirname, '../../server/models/users.js' ),
+      path.join(__dirname, '../../server/models/documents.js')
+    ]);
+
+    // Clear specified collections
+    seeder.clearModels(['User', 'Document'], function() {
+
+      // Callback to populate DB once collections have been cleared
+      seeder.populateModels(data);
+      // console.log('+++++++++', seeder.populateModels(data));
+
+    });
+  })
+
+};
