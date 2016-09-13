469184df2f9a76c477004310594e4465cd9b5f0d
Tonida Baraza <tonida.baraza@andela.com>
2016-09-13 10:00:26 +0300

    add bd config

--- config.js
+++ config.js
@@ -1,6 +1,22 @@
+const env = process.env.NODE_ENV || 'development';
+if (env === 'development') {
+  require('dotenv').load();
+}
+
+const config = {
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  host: process.env.DATABASE_HOST,
+  dialect: process.env.DATABASE_DIALECT,
+  port: process.env.DATABASE_PORT,
+  url: process.env.DATABASE_URL,
+  secret: process.env.SECRET_KEY
+};
+
 module.exports = {
-
-  'secret': 'IdontKnOw',
-  'database': 'mongodb://localhost/docsys-api'
-
+  'development': config,
+  'test': config,
+  'production': {'use_env_variable': 'DATABASE_URL'},
+  'staging': {'use_env_variable': 'DATABASE_URL'}
 };
