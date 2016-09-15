54ff42f705d2e09a8859a838db91c9a2f8adf4bb
Tonida Baraza <tonida.baraza@andela.com>
2016-09-15 11:51:33 +0300

    add user names validation test

--- test/server/users.spec.js
+++ test/server/users.spec.js
@@ -12,7 +12,7 @@ const should = require('should');
 before((done) => {
   const cb = () => {
     done();
-  }
+  };
   seed.seeder(cb);
 });
 
@@ -54,7 +54,7 @@ describe('User', () => {
           res.send(err);
           done();
         }
-        console.log(res.body);
+        // console.log(res.body);
         res.body.message.should.equal('user created');
         res.body.success.should.equal(true);
         res.body.user.should.have.property('role');
@@ -62,50 +62,37 @@ describe('User', () => {
         done();
       });
   });
-  //
-  // it('should validate that a new user created both first and last names', (done) => {
-  //   request(server)
-  //     .post('/api/users')
-  //     .send({
-  //       'firstname': 'Mark',
-  //       'lastname': 'Zuckerberg',
-  //       'username': 'zuck',
-  //       'email': 'toni@gmail.com',
-  //       'password': 'zuck'
-  //     })
-  //     .expect('Content-Type', /json/)
-  //     .expect(200)
-  //     .end((err, res) => {
-  //       console.log(res)
-  //       res.body.message.should.equal('user created');
-  //       res.body.success.should.equal(true);
-  //       res.body.user.should.have('firstname');
-  //       res.body.user.should.have('lastname');
-  //       res.status.should.equal(200);
-  //     });
-  //   done();
-  // });
-  //
-  // it('should validate that all users are returned', (done) => {
-  //   // request(server)
-  //   request.agent(app)
-  //     .get('/users')
-  //     .expect('Content-Type', /json/)
-  //     .end((err, res) => {
-  //       res.body.success.should.equal(true);
-  //       res.body.uses.should.equal({
-  //         "_id": ObjectId("57d85d4f90254a4521407d4e"),
-  //         "password": "$2a$10$ujQtWPzCLOvo9ARNEGtj.eCqUEMkt4YfXvBFj4YUl1bz/tbHQEZZG",
-  //         "email": "toni@gmail.com",
-  //         "username": "tonee",
-  //         "title": "user",
-  //         "name": {
-  //           "last": "baraza",
-  //           "first": "tonida"
-  //         },
-  //         "__v": 0
-  //       });
-  //     });
-  //   done();
-  // });
+
+  it('should validate that a new user created has both first and last names', (done) => {
+    request(server)
+      .post('/api/users')
+      .send({
+        firstname: 'Jacckline',
+        lastname: 'Kimani',
+        username: 'jacky',
+        email: 'jacky@gmail.com',
+        password: 'MK4'
+      })
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        // console.log(res.body)
+        res.body.message.should.equal('user created');
+        res.body.success.should.equal(true);
+        res.body.user.name.should.have.keys('firstname', 'lastname');
+        res.status.should.equal(200);
+        done();
+      });
+  });
+
+  it.skip('should validate that all users are returned', (done) => {
+    request(server)
+      .get('/users')
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        res.body.success.should.equal(true);
+        // res.body.uses.should.equal();
+        done();
+      });
+  });
 });
